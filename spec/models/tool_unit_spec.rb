66cf10cf43ca260c0775b62ae642d9dc99696905
matsuzakayuuta <yuutaapuppet0628@gmail.com>
2023-06-02 16:40:12 +0900

    【Add】モデルスペックを作成

---
+++ spec/models/tool_unit_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe ToolUnit do
+  describe 'バリデーションの確認' do
+    it '台番と差枚数も含めたツール出力結果が正常に登録される' do
+      tool = create(:tool)
+      tool.valid?
+      expect(tool).to be_valid
+    end
+
+    it '店舗名・パチスロ総設置台数が未入力の場合、台番・差枚数は登録できないこと' do
+      build(:tool, store_name: '', total_unit: '')
+      tool_unit = build(:tool_unit)
+      expect(tool_unit).to be_invalid
+    end
+
+    it '台番が未入力の場合、エラーが出ること' do
+      tool_unit = build(:tool_unit, number: '', medal: '1')
+      tool_unit.valid?
+      expect(tool_unit.errors[:number]).to include('を入力してください')
+    end
+
+    it '差枚が未入力の場合、エラーが出ること' do
+      tool_unit = build(:tool_unit, number: '1', medal: '')
+      tool_unit.valid?
+      expect(tool_unit.errors[:medal]).to include('を入力してください')
+    end
+
+    it '台番が255文字以上の場合、エラーが出ること' do
+      tool_unit = build(:tool_unit, number: ('1' * 256).to_i)
+      tool_unit.valid?
+      expect(tool_unit.errors[:number]).to include('は255文字以内で入力してください')
+    end
+
+    it '差枚が255文字以上の場合、エラーが出ること' do
+      tool_unit = build(:tool_unit, medal: ('1' * 256).to_i)
+      tool_unit.valid?
+      expect(tool_unit.errors[:medal]).to include('は255文字以内で入力してください')
+    end
+
+    it '台番が正の整数ではない場合、エラーが出ること' do
+      tool_unit = build(:tool_unit, number: '-1', medal: '1')
+      tool_unit.valid?
+      expect(tool_unit.errors[:number]).to include('は0以上の値にしてください')
+    end
+  end
+end
