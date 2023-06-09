f71109bb189210ff85662260c89f90d991edd934
matsuzakayuuta <yuutaapuppet0628@gmail.com>
2023-06-09 14:09:17 +0900

    【Add】ツール機能のシステムスペックファイルを作成

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,4 +2,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, screen_size: [360, 780]
   end
+  # config.before(:each, js: true, type: :system) do
+  # driven_by :selenium_chrome
+  # end
 end
